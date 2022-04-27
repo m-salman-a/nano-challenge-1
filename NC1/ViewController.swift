2c18c7d8b0f4498d838948375458c8b63342fb23
m salman a <mohammadalfarisi6@gmail.com>
2022-04-27 22:59:32 +0700

    Initial Commit

---
+++ NC1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NC1
+//
+//  Created by Mohammad Alfarisi on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
