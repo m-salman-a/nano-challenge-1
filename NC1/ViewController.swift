8ad9b884ca933319c5324d9848916fcd45d1f4f6
m salman a <mohammadalfarisi6@gmail.com>
2022-04-27 23:40:29 +0700

    feat: finish Main storyboard

--- NC1/ViewController.swift
+++ NC1/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wishlistTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        wishlistTableView.dataSource = self
+        wishlistTableView.delegate = self
     }
 
-
+    @IBAction func addNewItemAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func compareItemsAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addNewFolderAction(_ sender: Any) {
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
