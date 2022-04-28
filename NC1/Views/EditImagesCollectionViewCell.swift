06a933a56116927b6c80d9909fe9c056261aacd7
m salman a <mohammadalfarisi6@gmail.com>
2022-04-29 00:18:41 +0700

    feat: start add new item storyboard

---
+++ NC1/Views/EditImagesCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EditImagesCollectionViewCell.swift
+//  NC1
+//
+//  Created by Mohammad Alfarisi on 28/04/22.
+//
+
+import UIKit
+
+class EditImagesCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
